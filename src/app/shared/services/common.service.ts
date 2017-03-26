fb91c12251f8270d4feff2bbcdee9d3604340f9f
narendharlive <narendharlive@gmail.com>
2017-03-26 17:19:41 +0530

    'service:  Service Created Before working on services'

---
+++ src/app/shared/services/common.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import { of }         from 'rxjs/observable/of';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class CommonService {
+
+  constructor(private http: Http) { }
+
+  createAuthorizationHeader(headers: Headers) {
+      headers.append('Authorization', 'Basic ' + btoa('username:password'));
+      headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    }
+     getData(url) {
+        let headers = new Headers();
+        this.createAuthorizationHeader(headers);
+        return this.http.get(url, {headers: headers}).map((res:Response) => res.json())
+        .catch((error:any) => Observable.throw(error.json().error || 'Server error'));;
+      }
+
+      postData(url, data) {
+        let headers = new Headers();
+        this.createAuthorizationHeader(headers);
+        return this.http.post(url, data, {headers: headers}).map((res:Response) => res.json());
+       // .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+      }
+
+}
